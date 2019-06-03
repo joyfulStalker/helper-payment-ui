855cba67126c6e60aeedc962a13315c69e6feb5c
jokfulStalker <2030503816@qq.com>
2019-06-03 20:24:45 +0800

    分页查询

--- src/main/java/lsl/mint/config/MybatisPlusConfig.java
+++ src/main/java/lsl/mint/config/MybatisPlusConfig.java
@@ -1,14 +1,8 @@
 package lsl.mint.config;
 
 import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
-import com.baomidou.mybatisplus.extension.spring.MybatisSqlSessionFactoryBean;
-import org.apache.ibatis.plugin.Interceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
-
-import javax.sql.DataSource;
-import java.io.IOException;
 
 /**
  * @Desc TODO
@@ -19,16 +13,11 @@ import java.io.IOException;
 @Configuration
 public class MybatisPlusConfig {
 
+    /**
+     * 分页插件
+     */
     @Bean
-    public MybatisSqlSessionFactoryBean sqlSessionFactoryBean(DataSource dataSource) throws IOException {
-        MybatisSqlSessionFactoryBean sqlSessionFactoryBean = new MybatisSqlSessionFactoryBean();
-        sqlSessionFactoryBean.setDataSource(dataSource);
-        //mapper.xml扫描
-        sqlSessionFactoryBean.setMapperLocations(new PathMatchingResourcePatternResolver().getResources("classpath*:mapper/*Mapper.xml"));
-        //分页插件
-        PaginationInterceptor paginationInterceptor = new PaginationInterceptor();
-        Interceptor[] plugins = {paginationInterceptor};
-        sqlSessionFactoryBean.setPlugins(plugins);
-        return sqlSessionFactoryBean;
+    public PaginationInterceptor paginationInterceptor() {
+        return new PaginationInterceptor();
     }
 }
